d57c3b984a9a71d9858b04a723a0f79c40f0ccb4
Matthew Goldwater <mattgwater@gmail.com>
2016-12-05 15:12:27 -0800

    can render index.html on server

--- server/basic-server.js
+++ server/basic-server.js
@@ -1,25 +1,15 @@
-var http = require('http');
-var fs = require('fs');
-var server = http.createServer();
+//'/Users/student/Desktop/hrsf50-mvp/index.html'
 
-server.listen(8000);
-console.log('file runs');
-server.on('request', function(request, response) {
-  if (request.method === 'GET') {
-    if (request.url === '/') {
-      fs.readFile('/Users/student/Desktop/hrsf50-mvp/index.html', 'utf8', function(err,data){
-        if (err) {
-          throw err;
-        }
-        // console.log(data);
-        console.log('data is: ', data);
-        response.writeHead(200);
-        console.log('hits after writeHead');
-        response.end(data, 'utf-8');
-        console.log('hits after response.end');
-      });
-    }
-  } else {
-    response.end();
-  }
+var express = require('express');
+var app = express();
+
+app.listen(8000);
+app.use(express.static('client'));
+
+/*
+app.get('/', function(request, response) {
+  // response.send('<div>Hello World</div>');
+  response.render('../client/index', function() {
+  });
 });
+*/
